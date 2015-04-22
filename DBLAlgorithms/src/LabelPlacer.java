fce5ffe82ea3159b404db56e690555dd680e31cb
SjorsvandenElzen <Sjorsvandenelzen@ziggo.nl>
2015-04-22 13:07:54 +0200

    Improved the input of the points, added them to a vector list

--- DBLAlgorithms/src/LabelPlacer.java
+++ DBLAlgorithms/src/LabelPlacer.java
@@ -1,31 +1,38 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Vector;
 
-/**
- *
- * @author s132303
- */
 public class LabelPlacer {
+    
+    private static int width;
+    private static int height;
+    private static List<Vector> inputList;
    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> points = new ArrayList<>();
         
         String model = sc.nextLine().substring(16);
-        int width = Integer.parseInt(sc.nextLine().substring(7));
-        int height = Integer.parseInt(sc.nextLine().substring(8));
+        width = Integer.parseInt(sc.nextLine().substring(7));
+        height = Integer.parseInt(sc.nextLine().substring(8));
         int numberOfPoints = Integer.parseInt(sc.nextLine().substring(18));
+        
+        parseInput(sc);
+    }
+    
+    public static void parseInput(Scanner sc) {
         
         while(sc.hasNextLine()) {
             String line = sc.nextLine();
-            points.add(line);
-            System.out.println(line);
+            // Splitting the input by spaces, with the space regex
+            String[] split = line.split("\\s+");
+            inputList.add(  new Vector( Integer.parseInt(split[0]), 
+                                        Integer.parseInt(split[1])));
         }
         
     }
-    
+
 }

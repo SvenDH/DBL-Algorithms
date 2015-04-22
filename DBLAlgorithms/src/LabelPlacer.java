cb759a4ab3a5dd82c5839482fd5d655ca091c67c
Cryptorr <denhartog.sven@gmail.com>
2015-04-22 13:40:24 +0200

    Put everything in the main method

--- DBLAlgorithms/src/LabelPlacer.java
+++ DBLAlgorithms/src/LabelPlacer.java
@@ -4,26 +4,18 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class LabelPlacer {
-    
-    private static int width;
-    private static int height;
-    private static List<Vector> inputList;
    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);        
         
         String model = sc.nextLine().substring(16);
-        width = Integer.parseInt(sc.nextLine().substring(7));
-        height = Integer.parseInt(sc.nextLine().substring(8));
+        int width = Integer.parseInt(sc.nextLine().substring(7));
+        int height = Integer.parseInt(sc.nextLine().substring(8));
         int numberOfPoints = Integer.parseInt(sc.nextLine().substring(18));
-        
-        parseInput(sc);
-    }
-    
-    public static void parseInput(Scanner sc) {
+        List<Vector> inputList = new ArrayList<>();
         
         while(sc.hasNextLine()) {
             String line = sc.nextLine();
@@ -32,7 +24,5 @@ public class LabelPlacer {
             inputList.add(  new Vector( Integer.parseInt(split[0]), 
                                         Integer.parseInt(split[1])));
         }
-        
     }
-
 }

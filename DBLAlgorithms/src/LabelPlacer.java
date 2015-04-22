bfacd9bb3cb71c5cc2a5d0ba41121205f1f861a8
Cryptorr <denhartog.sven@gmail.com>
2015-04-22 12:54:24 +0200

    Better scanner

--- DBLAlgorithms/src/LabelPlacer.java
+++ DBLAlgorithms/src/LabelPlacer.java
@@ -7,17 +7,22 @@ import java.util.Scanner;
  * @author s132303
  */
 public class LabelPlacer {
-
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> input = new ArrayList<>();
+        List<String> points = new ArrayList<>();
+        
+        String model = sc.nextLine().substring(16);
+        int width = Integer.parseInt(sc.nextLine().substring(7));
+        int height = Integer.parseInt(sc.nextLine().substring(8));
+        int numberOfPoints = Integer.parseInt(sc.nextLine().substring(18));
         
         while(sc.hasNextLine()) {
             String line = sc.nextLine();
-            input.add(line);
+            points.add(line);
             System.out.println(line);
         }
         

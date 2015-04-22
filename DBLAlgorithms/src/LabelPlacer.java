9147508a1bd52e6255902a03b06098ebe4e73388
Cryptorr <denhartog.sven@gmail.com>
2015-04-22 10:57:49 +0200

    Placed main class outside package

--- DBLAlgorithms/src/LabelPlacer.java
+++ DBLAlgorithms/src/LabelPlacer.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;

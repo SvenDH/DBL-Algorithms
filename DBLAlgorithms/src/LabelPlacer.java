0ba03fc24941b6bd29da00e118c5be919913fab0
SjorsvandenElzen <Sjorsvandenelzen@ziggo.nl>
2015-04-29 15:50:56 +0200

    Refactored BinarySearchTree and altered LabelPlacer

--- DBLAlgorithms/src/LabelPlacer.java
+++ DBLAlgorithms/src/LabelPlacer.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class LabelPlacer {
+    
+    private static RangeSearch<Integer, PointData> rs;
+    private static List<PointData> inputList = new ArrayList<>();
+    private static int numberOfPoints;
    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);        
+    static void parseInput(Scanner sc) {
         String line;
         line = sc.nextLine();
         System.out.println(line);
@@ -23,9 +23,9 @@ public class LabelPlacer {
         int height = Integer.parseInt(line.substring(8));
         line = sc.nextLine();
         System.out.println(line);
-        int numberOfPoints = Integer.parseInt(line.substring(18));
+        numberOfPoints = Integer.parseInt(line.substring(18));
+        
         //Process points
-        List<PointData> inputList = new ArrayList<>();
         for (int i = 0; i < numberOfPoints; i++) {
             line = sc.nextLine();
             // Splitting the input by spaces, with the space regex
@@ -33,22 +33,35 @@ public class LabelPlacer {
             inputList.add( new PointData( Integer.parseInt(split[0]), 
                                         Integer.parseInt(split[1])));
         }
+    }
+    
+    static void buildRangeTree() {
+        rs = new RangeSearch<Integer, PointData>();
         
-        /*
-         * Todo: find optimal labels.
-         */
-        
-        RangeSearch<Integer, PointData> rs = new RangeSearch<Integer, PointData>();
-        
-        for (int i = 0; i < inputList.size(); i++)
+        for (int i = 0; i < inputList.size(); i++) {
             rs.put(i, inputList.get(i));
-        
+        }
+    }
+    
+    static void output() {
         int numberOfLabels = 0;
         System.out.println("number of labels: " + numberOfLabels);
         
         for (int i = 0; i < numberOfPoints; i++) {
             System.out.println(inputList.get(i).x + " " + inputList.get(i).y + " NIL");
         }
-
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        
+        parseInput(sc);
+        
+        /*
+         * Todo: find optimal labels.
+         */
+        buildRangeTree();
+        output();
     }
 }

5ba6faa2d1f6ea386fb1707af5c9f29f30ee2b60
Cryptorr <denhartog.sven@gmail.com>
2015-04-29 16:41:25 +0200

    PointData v2

--- DBLAlgorithms/src/PointData.java
+++ DBLAlgorithms/src/PointData.java
@@ -3,8 +3,17 @@ public class PointData {
     public int x;
     public int y;
     
+    boolean NW;
+    boolean NE;
+    boolean SW;
+    boolean SE;
+    
     public PointData (int x, int y) {
         this.x = x;
         this.y = y;
+        NW = true;
+        NE = true;
+        SW = true;
+        SE = true;
     }
 }

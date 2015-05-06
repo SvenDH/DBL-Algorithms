a516500e2b1cab7a86c23c30085cdd04831d3576
SjorsvandenElzen <Sjorsvandenelzen@ziggo.nl>
2015-05-06 11:12:02 +0200

    Implemented bruteforce
    without collision checking

--- DBLAlgorithms/src/BruteForceSolver.java
+++ DBLAlgorithms/src/BruteForceSolver.java
@@ -7,21 +7,84 @@ public class BruteForceSolver implements LabelSolver {
     
     private final int width;
     private final int height;
-    private Stack<Command> commands = new Stack<Command>();
-    private int maxLabels = 0;
+    private BruteForceSolver instance;
     
-    BruteForceSolver (int width, int height) {
+    private BruteForceSolver (int width, int height) {
         this.width = width;
         this.height = height;
     }
-
-    @Override
-    public void getLabeledPoints(List<PointData> points) {
-        
+    
+    public BruteForceSolver getInstance() {
+        return this.instance;
     }
     
-    private void getMaxLabels(Stack<Command> commandStack) {
-        
+    public BruteForceSolver getInstance(int width, int height) {
+        return new BruteForceSolver(width, height);
     }
 
+    /**
+     * First getMaxLabels is called to fill the stack.
+     * All that's left after is to use the stack stored 
+     * in the instance of BruteForceSolver to execute the commands which lead
+     * to the ideal placement of labels.
+     * @param points 
+     */
+    @Override
+    public void getLabeledPoints(List<PointData> points) {
+        Stack<Command> commands = getMaxLabels(new Stack<Command>(), 0, 0, points);
+        while (!commands.empty()) {
+            Command command = commands.pop();
+            command.execute();
+        }
+    }
+  
+    private Stack<Command> getMaxLabels(Stack<Command> commandStack, int labels, int maxLabels, List<PointData> points) {
+        if (maxLabels == points.size()) {
+            return commandStack;
+        }
+        List<PossibleLabel> possibleLabels = getPossibleLabels(points);
+        for (PossibleLabel possibleLabel : possibleLabels) {
+            maxLabels++;
+            if (labels > maxLabels) {
+                commandStack.add(new PlaceLabelCommand(
+                        possibleLabel.getPoint(), 
+                        possibleLabel.getVertical(), 
+                        possibleLabel.getHorizontal(), width, height));
+                return getMaxLabels(commandStack, labels, labels, points);
+            } else {
+                maxLabels--;
+            }
+        }
+        return null;
+    }
+    
+    private List<PossibleLabel> getPossibleLabels(List<PointData> points) {
+        
+        return null;
+    }
+
+}
+
+class PossibleLabel {
+    private PointData point;
+    private int verticalDirection;
+    private int horizontalDirection;
+    
+    public PossibleLabel(PointData point, int verticalDirection, int horizontalDirection) {
+        this.point = point;
+        this.verticalDirection = verticalDirection;
+        this.horizontalDirection = horizontalDirection;
+    }
+     
+    public PointData getPoint() {
+        return point;
+    }
+    
+    public int getVertical() {
+        return verticalDirection;
+    }
+    
+    public int getHorizontal() {
+        return horizontalDirection;
+    }
 }
